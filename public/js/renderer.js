336a04d8c1275dab3c963d6a0f542f1777e46aeb
--global <--global>
2024-10-05 02:06:03 -0300

    dev fix

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -1,29 +1,30 @@
 import * as THREE from 'three';
 import { OrbitControls } from '../three/controls/OrbitControls.js';
+import { Lensflare, LensflareElement } from '../three/objects/Lensflare.js';
 
-let hemiLight, skyboxGeo, skybox, clouds, waterworld
+let hemiLight, skyboxGeo, skybox, clouds, waterworldmesh,sun,date
+let orbitRadius = 12000
 
 var scene = new THREE.Scene();
-
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 15000 );
+var textureLoader = new THREE.TextureLoader();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 70000 );
 camera.position.z = 10;
 
 var renderer = new THREE.WebGLRenderer();
 
 var controls = new OrbitControls( camera, renderer.domElement );
-controls.enablePan = false;
-controls.target.set( 0, 0, 0 );
+controls.enablePan = true;
+controls.target.set(0, 0, 12000);
 controls.minDistance = 100;
-controls.maxDistance = 300.0;
+controls.maxDistance = 300;
 controls.update();
 
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// WaterWorld
 var waterWorldGeometry = new THREE.SphereGeometry( 30,60,60 );
-
-var textureLoader = new THREE.TextureLoader();
 
 var material = new THREE.MeshPhongMaterial ( 
   { 
@@ -43,9 +44,7 @@ var material = new THREE.MeshPhongMaterial (
 
 );
 
-waterworld = new THREE.Mesh( waterWorldGeometry, material );
-
-scene.add(waterworld)
+waterworldmesh = new THREE.Mesh( waterWorldGeometry, material );
 
 var customMaterial = new THREE.ShaderMaterial( 
 	{
@@ -63,9 +62,7 @@ var customMaterial = new THREE.ShaderMaterial(
 		transparent: true
 	}   );
 var glowGeometry = new THREE.SphereGeometry(32, 64, 64)
-var moonGlow = new THREE.Mesh( glowGeometry,customMaterial);
-moonGlow.position.set(0,0,0)
-scene.add( moonGlow );
+var atmosphereGlow = new THREE.Mesh( glowGeometry,customMaterial);
 
 let cloudGeo = new THREE.SphereGeometry(30.5, 64, 64)
 let cloudsMat = new THREE.MeshStandardMaterial({
@@ -74,12 +71,52 @@ let cloudsMat = new THREE.MeshStandardMaterial({
 })
 clouds = new THREE.Mesh(cloudGeo, cloudsMat)
 
-scene.add(clouds)
+const waterworld = new THREE.Group();
+waterworld.add(waterworldmesh)
+waterworld.add(clouds)
+waterworld.add(atmosphereGlow)
+waterworld.position.set(0, 0, 12000)
+scene.add(waterworld)
 
-const pointLight = new THREE.PointLight( 0xffffff, 10000, 2000, 2);
-pointLight.position.set( 0, 50, 100 );
-scene.add( pointLight );
+// Sun
+var sunGeometry = new THREE.SphereGeometry(1000,60,60);
 
+var sunMaterial = new THREE.MeshBasicMaterial ( 
+  { 
+    map: textureLoader.load('../three/textures/sunmap.jpg'),
+    color: 0xFFA500,
+  } 
+
+);
+
+sun = new THREE.Mesh(sunGeometry, sunMaterial)
+
+var sunLight = new THREE.PointLight( 0xffffff, 10000, 15000, 1);
+
+var sunLightGlow = new THREE.ShaderMaterial( 
+	{
+	    uniforms: 
+		{ 
+			"c":   { type: "f", value: 0.9 },
+			"p":   { type: "f", value: 4.0 },
+			glowColor: { type: "c", value: new THREE.Color(0xFFA500) },
+			viewVector: { type: "v3", value: camera.position }
+		},
+		vertexShader:   document.getElementById( 'vertexShader'   ).textContent,
+		fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+		side: THREE.BackSide,
+		blending: THREE.AdditiveBlending,
+		transparent: true
+	}   );
+var sunGlowGeometry = new THREE.SphereGeometry(1060, 64, 64)
+var sunGlow = new THREE.Mesh( sunGlowGeometry,sunLightGlow);
+
+const sunGroup = new THREE.Group();
+sunGroup.add(sunGlow)
+sunGroup.add(sunLight)
+sunGroup.add(sun)
+sunGroup.position.set(0, 1000, 0)
+scene.add(sunGroup)
 
 hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1 );
 hemiLight.color.setHSL( 0.6, 1, 0.6 );
@@ -100,11 +137,12 @@ skyBoxMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
 skyBoxMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
 skyBoxMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
 skyBoxMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+
 for (let i = 0; i < 6; i++) {
   skyBoxMaterialArray[i].side = THREE.BackSide;
 }
 
-skyboxGeo = new THREE.BoxGeometry( 15000, 15000, 15000);
+skyboxGeo = new THREE.BoxGeometry(  70000, 70000, 70000);
 skybox = new THREE.Mesh( skyboxGeo, skyBoxMaterialArray );
 scene.add( skybox );
 skybox.position.set(0,0,0)
@@ -114,9 +152,16 @@ skybox.position.set(0,0,0)
 
 function animate() {
 	requestAnimationFrame( animate );
-  const time = performance.now() * 0.001;
-  // moon.rotation.y += 0.002;
-  // moon.rotation.x += 0.0001;
+  waterworldmesh.rotation.y += -0.000090;
+  waterworldmesh.rotation.x += -0.000090;
+  date = Date.now() * 0.0001;
+  waterworld.position.set(
+    Math.cos(date) * orbitRadius,
+    0,
+    Math.sin(date) * orbitRadius
+  );
+  controls.target.set(waterworld.position.x, 0, waterworld.position.z);
+  controls.update();
   clouds.rotateY(0.00020)
 	renderer.render( scene, camera );
 }
